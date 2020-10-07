f321ac7ca91c0c5ca7797592bff47ff2dd79b47c
debugleader.abdallah <aboudi41444@gmail.com>
2020-10-07 10:55:05 +0400

    Updates

---
+++ precache-manifest.840f23fe570d44c39500eaa3851f7fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81be49767f19bb88f136047e338b3582",
+    "url": "/RecipeX/index.html"
+  },
+  {
+    "revision": "c7d04b0a0a6f0a99d0b9",
+    "url": "/RecipeX/static/css/main.bd98a94c.chunk.css"
+  },
+  {
+    "revision": "e6607ab21eaf90bf52fd",
+    "url": "/RecipeX/static/js/2.b51a9c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeX/static/js/2.b51a9c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d04b0a0a6f0a99d0b9",
+    "url": "/RecipeX/static/js/main.d241fc5b.chunk.js"
+  },
+  {
+    "revision": "66d79c0db21684999557",
+    "url": "/RecipeX/static/js/runtime-main.175907a0.js"
+  }
+]);
